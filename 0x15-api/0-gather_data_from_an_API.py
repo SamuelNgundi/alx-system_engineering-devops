215907ebe6fa1b5b67c4f30d4779187d187fdd73
SamuelNgundi <samuelwngundi02@gmail.com>
2022-10-19 20:46:28 +0300

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-"""For a given employee ID, return information about
-their TODO list progress"""
-
+"""
+    Api usage
+"""
 import requests
-import sys
+from sys import argv, exit
+
 
 if __name__ == "__main__":
+    try:
+        if len(argv) < 2 or type(eval(argv[1])) is not int:
+            exit(1)
+    except Exception:
+        exit(1)
 
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
+    count = 1
+    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        argv[1]
+    )
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        argv[1]
+    )
+    # get user's name
+    user = requests.get(user_url)
+    user_name = user.json().get('name')
 
-    name = user.json().get('name')
+    # get tasks done by user
+    todo = requests.get(todo_url)
+    task_len = len(todo.json())
+    comp_task = sum([count for attr in todo.json()
+                     if attr.get('completed') is True])
 
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-    totalTasks = 0
-    completed = 0
-
-    for task in todos.json():
-        if task.get('userId') == int(userId):
-            totalTasks += 1
-            if task.get('completed'):
-                completed += 1
-
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, completed, totalTasks))
-
-    print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('userId') == int(userId) and task.get('completed')]))
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user_name, comp_task, task_len
+    ))
+    for attr in todo.json():
+        if attr.get('completed') is True:
+            print('\t {}'.format(attr.get('title')))
